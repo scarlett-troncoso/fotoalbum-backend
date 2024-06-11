62b0af0ef2819e87619160b13a0105a87126ca68
scarlett-troncoso <scar.labussieri@gmail.com>
2024-06-11 21:53:40 +0200

    add properties for validations

--- app/Http/Requests/StorePhotoRequest.php
+++ app/Http/Requests/StorePhotoRequest.php
@@ -11,7 +11,7 @@ class StorePhotoRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class StorePhotoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required',
+            'upload_image' => 'nullable|image',
+            'description' => 'nullable'
         ];
     }
 }
