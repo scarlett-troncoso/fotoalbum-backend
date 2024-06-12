03363902d4207027975ec8aa8a181e38e9c46be8
scarlett-troncoso <scar.labussieri@gmail.com>
2024-06-12 13:08:33 +0200

    add relation one to many of category and photo

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Photo extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'upload_image', 'description', 'slug'];
+    protected $fillable = ['title', 'upload_image', 'description', 'slug', 'category_id'];
+
+
+    /**
+     * Get the category that owns the Photo
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo //Una photo appartiene a una category
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
